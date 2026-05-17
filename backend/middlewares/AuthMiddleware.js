c5bfbee82aae91796a7c67faf3c438b71782380d
dibaya07 <dibyajyotibaruah987@gmail.com>
2026-05-17 13:02:26 +0530

    error fix

--- backend/middlewares/AuthMiddleware.js
+++ backend/middlewares/AuthMiddleware.js
@@ -4,13 +4,17 @@ const jwt = require("jsonwebtoken");
 
 module.exports.userVerification = (req, res) => {
   const token = req.cookies.token
+  console.log("token",token)
   if (!token) {
+    console.log("didn't get token")
     return res.json({ status: false })
   }
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
     if (err) {
+      console.log("got err", err)
      return res.json({ status: false })
     } else {
+      console.log("got user")
       const user = await User.findById(data.id)
       if (user) return res.json({ status: true, user: user.username })
       else return res.json({ status: false })
